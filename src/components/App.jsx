3c5f5c23bf177023703cda43d2175f90ec832cb4
zganz <humanmailer@gmail.com>
2022-07-25 14:59:43 +0300

    add hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,30 +10,48 @@ export function App() {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  onButtonClick = event => {
+  const onButtonClick = event => {
     const clickTarget = event.target.textContent.toLowerCase();
+    if (clickTarget === 'good') {
+      setGood(good + 1);
+    } else if (clickTarget === 'neutral') {
+      setNeutral(neutral + 1);
+    } else if (clickTarget === 'bad') {
+      setBad(bad + 1);
+    }
+  };
+
+  const countTotalFeedback = () => {
+    let total = 0;
+    total = good + neutral + bad;
+    return total;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    let percentage = Math.round((good / countTotalFeedback()) * 100);
+    return percentage;
   };
 
   return (
     <div>
       <Section title="Оставьте отзыв">
         <FeedbackOptions
-          // options={Object.keys(this.state)}
-          onLeaveFeedback={this.onButtonClick}
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={onButtonClick}
         />
       </Section>
       <Section title="Cтатистика">
-        {/* {this.countTotalFeedback() === 0 ? (
+        {countTotalFeedback() === 0 ? (
           <Notification message="There is no feedback" />
         ) : (
           <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={this.countTotalFeedback()}
-            positivePercentage={this.countPositiveFeedbackPercentage()}
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-        )} */}
+        )}
       </Section>
     </div>
   );
